c1b2fe22f48fb4bc0acb9ecf3bcbb4a338c00fde
SabbirDG001 <sabbiras75ag@gmail.com.com>
2021-09-26 23:06:52 +0600

    final

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,19 +4,23 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { personCart } = props;
-    console.log(personCart);
+    const length = personCart.length;
     const totalReducer = (previous, person) => previous + person.balance;
     const total = personCart.reduce(totalReducer, 0);
     return (
         <div>
             <div className="container">
-                <h2>Total added: <span>{personCart.length}</span></h2>
+                <h2>Total added: <span>{length}</span></h2>
                 <h2>Total cost: <span>{total}</span></h2>
             </div>
             <div>
                 <h3>Added persons:</h3>
                 <div>
-                    
+                    <ul>
+                    {
+                        personCart.map(props=><li>{props.name}</li>)
+                    }
+                    </ul>
                 </div>
             </div>
         </div>
